8ee122d69abb58efedc556865f768fc0f8e09309
Dhanendra Kumar <dhanendrakumarkht@gmail.com>
2016-10-18 15:14:52 +0530

    Added support for showing title from <option> element.

--- jquery.sumoselect.js
+++ jquery.sumoselect.js
@@ -155,6 +155,9 @@
                     if (opt.attr('class'))
                         li.addClass(opt.attr('class'));
 
+                    if (opt.attr('title'))
+                        li.attr('title', opt.attr('title'));
+
                     return li;
                 },
 
