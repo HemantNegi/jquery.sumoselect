71524af391bbad55de5239129982f137926b2854
Zenoo <perso@franckdemoute.fr>
2021-06-17 20:10:54 +0200

    fix: CSS update on release + Remove useless files

--- release.bash
+++ release.bash
@@ -2,15 +2,21 @@
 shx cp src/jquery.sumoselect.js ./
 
 # Generate IE friendly minified file at root
-babel src -o jquery.sumoselect.min.js
+babel jquery.sumoselect.js -o jquery.sumoselect.min.js
 
-# Copy minified file in docs
+# Generate minified CSS
+minify sumoselect.css > sumoselect.min.css
+
+# Copy minified files in docs
 shx cp jquery.sumoselect.min.js docs/javascripts
+shx cp sumoselect.min.css docs/stylesheets
 
 # Commit those changes
 git add docs/javascripts/jquery.sumoselect.min.js
+git add docs/stylesheets/sumoselect.min.css
 git add jquery.sumoselect.min.js 
 git add jquery.sumoselect.js
+git add sumoselect.min.css
 git diff-index --quiet HEAD || git commit -m "chore: Files minification"
 
 # Release
