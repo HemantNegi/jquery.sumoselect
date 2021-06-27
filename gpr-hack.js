1fbbf2768dfbf7d656f926efcf8a2a385e89bdfd
Zenoo <perso@franckdemoute.fr>
2021-06-27 12:07:12 +0200

    admin: Github package release script

---
+++ gpr-hack.js
@@ -0,0 +1,11 @@
+import { writeFileSync, readFileSync } from "fs";
+
+const file = readFileSync("./package.json", {
+  encoding: "utf-8",
+});
+
+const json = JSON.parse(file);
+
+json.name = "@zenoo/jquery.sumoselect";
+
+writeFileSync("./package.json", JSON.stringify(json, undefined, 2));
